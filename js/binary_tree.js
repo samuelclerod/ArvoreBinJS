04a055f5dcd2ba22446c2410ae2a4af7440b6eb6
samuelclerod <samuelclerod@gmail.com>
2018-11-05 21:44:05 -0300

    implementado método incluir e buscar

--- js/binary_tree.js
+++ js/binary_tree.js
@@ -1,8 +1,33 @@
 class BinaryTree{
     constructor(){
+        this.root = null;
+    }
+
+    insertNode(root, node){
+        if(node.content<root.content){
+            //inserir na esquerda
+            if(root.left===null){
+                root.left=node;
+            }else{
+                this.insertNode(root.left, node);
+            }
+        }else{
+            //inserir na direita
+            if(root.right===null){
+                root.right=node;
+            }else{
+                this.insertNode(root.right, node);
+            }
+        }
     }
 
     insert(element){
+        let node = new node(element);
+        if(this.root===null){
+            this.root = node;
+        }else{
+            this.insertNode(this.root, node);
+        }
     }
 
     inOrderTraverse(callback){
@@ -15,6 +40,18 @@ class BinaryTree{
     }
 
     search(value){
+        return this.searchNode(this.root, value);
+    }
+    searchNode(root, value){
+        if(root===null)
+            return false;
+        if(value>root.content){
+            return this.searchNode(root.right, value);
+        }else if(value<root.content){
+            return this.searchNode(root.left, value);
+        }else{
+            return true;
+        }
     }
 
     remove(value){
