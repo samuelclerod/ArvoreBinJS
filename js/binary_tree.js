1282a662a42a2d9e4c4d77c3c16116ad91e1356a
samuelclerod <samuelclerod@gmail.com>
2018-11-14 21:42:33 -0300

    concluida a implementação da arvore

--- js/binary_tree.js
+++ js/binary_tree.js
@@ -97,12 +97,51 @@ class BinaryTree{
     }
 
     remove(value){
+        this.root = this.removeNode(this.root, value);
+    }
+    removeNode(node, value){
+        if(node===null)
+            return null;
+        if(value>node.content){
+            node.right = this.removeNode(node.right, value);
+        }else if(value<node.content){
+            node.left = this.removeNode(node.left, value);
+        }else{
+            if(node.right===node.left){
+                return null;
+            }else if(node.right===null){
+                return node.left;
+            }else if(node.left===null){
+                return node.right;
+            }else{
+                let i=node.right;
+                while(i.left) i=i.left;
+                i.left = node.left;
+                return node.right;
+            }
+        }
     }
 
     heigth(){
+        return this.heigthNode(this.root)
+    }
+    heigthNode(node){
+        if(node==null) return -1
+        let l = this.heigthNode(node.left),
+            r = this.heigthNode(node.right);
+        if(l>r)
+            return l+1;
+        else
+            return r+1
     }
 
+
     size(){
+        return this.sizeNode(this.root);
+    }
+    sizeNode(node){
+        if(node===null) return 0;
+        return 1 + this.sizeNode(node.left)+this.sizeNode(node.right); 
     }
 
 }
