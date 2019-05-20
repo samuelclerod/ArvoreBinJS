cf09a0fe4ef7ff20db2f38a473b971c0aabb4edc
samuelclerod <samuelclerod@gmail.com>
2019-05-20 19:45:03 -0300

    Projeto inicial comentado

--- js/binary_tree.js
+++ js/binary_tree.js
@@ -1,34 +1,45 @@
 class BinaryTree{
+    // inicializa a raiz como nula
     constructor(){
     }
 
+    //exibe o menor valor da arvore
     min(){
     }
 
+    //exibe o maior valor da arvore
     max(){
     }
 
+    //insere o elemento da arvores
     insert(element){
     }
 
+    //executa a função callback para cada nó, em ordem
     inOrderTraverse(callback){
     }
 
+    //executa a função callback para cada nó, em pré-ordem
     preOrderTraverse(callback){
     }
     
+    //executa a função callback para cada nó, em pós-ordem
     postOrderTraverse(callback){
     }
 
+    //retorna true se o valor já existe na arvore
     search(value){
     }
     
+    //remove um elemento existente na arvore o retorna
     remove(value){
     }
 
+    //exibe a altura da arvore
     heigth(){
     }
 
+    // informa quantos nós existem na arvore
     size(){
     }
 
