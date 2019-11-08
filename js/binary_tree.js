7de1fc8e00042d6b7630acddb30602f76c617163
Samuel Rodrigues <samuelclerod@gmail.com>
2019-11-07 22:05:36 -0300

    iniciada a implementação da arvore

--- js/binary_tree.js
+++ js/binary_tree.js
@@ -1,46 +1,75 @@
-class BinaryTree{
+class BinaryTree {
     // inicializa a raiz como nula
-    constructor(){
+    constructor() {
+        this.root = null;
     }
 
     //exibe o menor valor da arvore
-    min(){
-    }
+    min() { }
 
     //exibe o maior valor da arvore
-    max(){
-    }
+    max() { }
 
     //insere o elemento da arvores
-    insert(element){
+    insert(element) {
+        this.root = this.insertNode(this.root, element);
+    }
+
+    insertNode(node, element) {
+        if (node == null)
+            return new Node(element);
+        if (element < node.content)
+            node.left = this.insertNode(node.left, element);
+        else
+            node.right = this.insertNode(node.right, element);
+        return node;
     }
 
     //executa a função callback para cada nó, em ordem
-    inOrderTraverse(callback){
+    inOrderTraverse(callback) {
+        this.inOrderTraverseNode(this.root, callback);
+    }
+    inOrderTraverseNode(node, callback) {
+        if (node == null) return;
+        this.inOrderTraverseNode(node.left, callback);
+        callback(node.content);
+        this.inOrderTraverseNode(node.right, callback);
     }
 
     //executa a função callback para cada nó, em pré-ordem
-    preOrderTraverse(callback){
+    preOrderTraverse(callback) {
     }
-    
+
     //executa a função callback para cada nó, em pós-ordem
-    postOrderTraverse(callback){
+    postOrderTraverse(callback) {
     }
 
     //retorna true se o valor já existe na arvore
-    search(value){
+    search(value) {
+        return this.searchNode(this.root, value);
     }
-    
+
+    searchNode(node, value) {
+        if (node == null)
+            return false;
+        if (node.content == value)
+            return true;
+        if (value < node.content)
+            return this.searchNode(node.left, value);
+        else
+            return this.searchNode(node.right, value);
+    }
+
     //remove um elemento existente na arvore o retorna
-    remove(value){
+    remove(value) {
     }
 
     //exibe a altura da arvore
-    heigth(){
+    heigth() {
     }
 
     // informa quantos nós existem na arvore
-    size(){
+    size() {
     }
 
 }
