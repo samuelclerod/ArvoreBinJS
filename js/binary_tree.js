2e0f6db380e18461d9bc3c92fc3b80703655c79e
Samuel Rodrigues <samuelclerod@gmail.com>
2020-06-04 21:43:54 -0300

    Implementado o inserir

--- js/binary_tree.js
+++ js/binary_tree.js
@@ -1,46 +1,67 @@
-class BinaryTree{
+class BinaryTree {
     // inicializa a raiz como nula
-    constructor(){
+    constructor() {
+        this.root = null
     }
 
     //exibe o menor valor da arvore
-    min(){
+    min() {
     }
 
     //exibe o maior valor da arvore
-    max(){
+    max() {
     }
 
     //insere o elemento da arvores
-    insert(element){
+    insert(element) {
+        this.root = this.insertNode(this.root, element)
+    }
+
+    insertNode(rootNode, element) {
+        if (rootNode == null)
+            return new Node(element)
+        if (element > rootNode.content)
+            rootNode.right = this.insertNode(rootNode.right, element)
+        else
+            rootNode.left = this.insertNode(rootNode.left, element)
+        return rootNode
     }
 
     //executa a função callback para cada nó, em ordem
-    inOrderTraverse(callback){
+    inOrderTraverse(callback) {
+        this.inOrderVisitor(this.root, callback)
+    }
+
+    inOrderVisitor(node, callback) {
+        if (node == null)
+            return
+        this.inOrderVisitor(node.left, callback)
+        callback(node.content)
+        this.inOrderVisitor(node.right, callback)
     }
 
     //executa a função callback para cada nó, em pré-ordem
-    preOrderTraverse(callback){
+    preOrderTraverse(callback) {
     }
-    
+
     //executa a função callback para cada nó, em pós-ordem
-    postOrderTraverse(callback){
+    postOrderTraverse(callback) {
     }
 
     //retorna true se o valor já existe na arvore
-    search(value){
+    search(value) {
     }
-    
+
     //remove um elemento existente na arvore o retorna
-    remove(value){
+    remove(value) {
     }
 
     //exibe a altura da arvore
-    heigth(){
+    heigth() {
     }
 
     // informa quantos nós existem na arvore
-    size(){
+    size() {
     }
 
 }
