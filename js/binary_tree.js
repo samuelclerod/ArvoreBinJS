156b55ec12b8195f9bb4a290adff602eea50d2be
Samuel Rodrigues <samuelclerod@gmail.com>
2021-05-18 19:30:22 -0300

    Implementado inOrderTraverse, search

--- js/binary_tree.js
+++ js/binary_tree.js
@@ -6,23 +6,30 @@ class BinaryTree {
 
     //DONE insere o elemento da arvores
     insert(element) {
-        this.root = this.recursive_insertion(this.root, element);
+        this.root = this.recursiveInsertion(this.root, element);
     }
-    recursive_insertion(node, element) {
+    recursiveInsertion(node, element) {
         if (node === null) {
             node = new Node(element);
         } else if (element < node.content) {
-            node.left = this.recursive_insertion(node.left, element);
+            node.left = this.recursiveInsertion(node.left, element);
         } else {
-            node.right = this.recursive_insertion(node.right, element);
+            node.right = this.recursiveInsertion(node.right, element);
         }
         return node
     }
 
-
-
     //TODO executa a função callback para cada nó, em ordem
     inOrderTraverse(callback) {
+        this.inOrderRecursive(this.root, callback)
+    }
+
+    //left content right
+    inOrderRecursive(node, cb) {
+        if (!node) return;
+        this.inOrderRecursive(node.left, cb);
+        cb(node.content);
+        this.inOrderRecursive(node.right, cb)
     }
 
     //TODO executa a função callback para cada nó, em pré-ordem
@@ -35,6 +42,17 @@ class BinaryTree {
 
     //TODO retorna true se o valor já existe na arvore
     search(value) {
+        return this.recursiveSearch(this.root, value);
+    }
+
+    recursiveSearch(node, value) {
+        if (node === null)
+            return false;
+        if (node.content === value)
+            return true;
+        if (value < node.content)
+            return this.recursiveSearch(node.left, value);
+        return this.recursiveSearch(node.right, value)
     }
 
     //TODO remove um elemento existente na arvore o retorna
